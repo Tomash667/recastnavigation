e7cab2b96d5dc637d0a5b4e3f98b221384b70372
Tomashu <tomash4my@gmail.com>
2018-07-15 21:51:24 +0200

    demo project for vs2017

--- RecastDemo/Source/Sample.cpp
+++ RecastDemo/Source/Sample.cpp
@@ -169,10 +169,10 @@ void Sample::collectSettings(BuildSettings& settings)
 
 void Sample::resetCommonSettings()
 {
-	m_cellSize = 0.3f;
-	m_cellHeight = 0.2f;
-	m_agentHeight = 2.0f;
-	m_agentRadius = 0.6f;
+	m_cellSize = 0.15f;
+	m_cellHeight = 0.25f;
+	m_agentHeight = 1.73f;
+	m_agentRadius = 0.3f;
 	m_agentMaxClimb = 0.9f;
 	m_agentMaxSlope = 45.0f;
 	m_regionMinSize = 8;
